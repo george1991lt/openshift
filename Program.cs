677a14db07defd724cf5d815ea919ccbaeac6161
george1991lt <46921435+george1991lt@users.noreply.github.com>
2019-01-22 12:48:22 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,19 +11,15 @@ using Microsoft.AspNetCore.Builder;
 
 namespace WebApplication1
 {
-    var url = config["ASPNETCORE_URLS"] ??
-        "http://*:8080";
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-            ConfigurationBuilder().AddEnvironmentVariables("").Build();
+            
         }
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls(url)
                 .Build();
     }
 }
